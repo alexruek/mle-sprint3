b0a432df384a9e1a3dca7de670a63ca5868ba02e
alexruek <a@pavlyuk.net>
2025-01-11 20:21:28 +0000

    2

--- app/main.py
+++ app/main.py
@@ -17,3 +17,18 @@ def read_root():
 @app.get("/api/churn/{user_id}")
 def get_prediction_for_item(user_id: str):
     return {"user_id": user_id, "score": random.random()}
+
+# обработка GET-запросов к URL /service-status
+@app.get("/service-status")
+def health_check():
+    return {"status": "ok"}
+
+# обрабатываем GET-запросы по пути /api/credit/{client_id},
+# чтобы узнать, выдать кредит или нет на основании кредитного score клиента
+@app.get("/api/credit/{client_id}")
+def is_credit_approved(client_id: int):
+    score = random.uniform(0, 1)  # генерируем случайный score от 0 до 1
+    if score > 0.8:
+        return {"approved": 1}
+    else:
+        return {"approved": 0}
